aeeb895c4d71e74aed870a594015417ae6d7d30d
Enverus <enver.abdullaev@onlinesup.com>
2024-02-29 18:28:09 +0300

    alpha v2

--- core/settings.py
+++ core/settings.py
@@ -110,22 +110,22 @@ if DB_ENGINE and DB_NAME and DB_USERNAME:
         }, 
     }
 else:
-    # DATABASES = {
-    #     'default': {
-    #         'ENGINE': 'django.db.backends.mysql',
-    #         'NAME': 'django',
-    #         'USER': 'root',
-    #         'PASSWORD': 'deadok',
-    #         'HOST': 'localhost',
-    #         'PORT': '3306',
-    #     }
-    # }
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': 'db.sqlite3'
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'django',
+            'USER': 'root',
+            'PASSWORD': 'deadok',
+            'HOST': 'localhost',
+            'PORT': '3306',
         }
     }
+    # DATABASES = {
+    #     'default': {
+    #         'ENGINE': 'django.db.backends.sqlite3',
+    #         'NAME': 'db.sqlite3'
+    #     }
+    # }
 
 MEDIA_ROOT = '/media'
 
